065193d43341624267ea5d46c07736b6ad9487e3
mexwew <m.hennings@freenet.de>
2018-05-19 17:29:38 +0200

    Impressum erstellt

---
+++ templates/impressum.php
@@ -0,0 +1,6 @@
+<div id="page">
+  <h1>Impressum</h1>
+  <a href="#menu">Menu</a>
+
+  <h1>Alea jacta est</h1>
+</div>
---
+++ templates/rules.php
@@ -0,0 +1,6 @@
+<div id="page">
+  <h1>Spielregeln</h1>
+  <a href="#menu">Menu</a>
+
+  <h1>In dubio pro reum</h1>
+</div>
