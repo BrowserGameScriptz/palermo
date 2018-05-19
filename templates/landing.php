bcddb32fe3720e2026d1a2ceeecc152db8128c6e
mexwew <m.hennings@freenet.de>
2018-05-19 15:58:16 +0200

    1

---
+++ templates/landing.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <title>Mord in Palermo</title>
+  </head>
+  <body>
+    <h1>Mord in Palermo</h1>
+
+
+
+  </body>
+</html>
