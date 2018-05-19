7867556e2df7aae50de998f5c1f9a5e18f8883db
Marcel Haupt <subcortexx@gmail.com>
2018-05-19 16:53:30 +0200

    Multiple Sites Index

--- index.php
+++ index.php
@@ -4,7 +4,29 @@ require "class/templates.php";
 
 $_PAGE['title'] = "Mord In Palermo";
 
+
+
+
 include "templates/header.php";
-include "templates/landing.php";
+
+if (!array_key_exists('p', $_GET)) {
+  $_GET['p'] =0;
+}
+
+switch(intval($_GET['p'])) {
+  case 2:
+    include "templates/aboutus.php";
+    break;
+
+  case 3:
+    include "templates/aboutus.php";
+    break;
+
+  default:
+    include "templates/landing.php";
+    break;
+}
+
+include "templates/menu.php";
 include "templates/footer.php";
 ?>
--- templates/landing.php
+++ templates/landing.php
@@ -1,17 +1,9 @@
 <div id="page">
-  <h1>Mord in Palermo</h1>
+  <div id="header">
+    <h1>Mord in Palermo</h1>
+  </div>
+
+
   <a href="#menu">Menu</a>
   <button type="button" class="btn btn-dark">Neues Spiel</button>
 </div>
-
-
-
-<!-- The menu -->
-<nav id="menu">
-   <ul>
-      <li><a href="#">Start</a></li>
-      <li><a href="#">Spielregeln</a></li>
-      <li><a href="#">Über Uns</a></li>
-      <li><a href="#">Impressum</a></li>
-   </ul>
-</nav>
