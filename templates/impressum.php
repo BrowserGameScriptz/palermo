ac212e942b88367e66a5918fd1366b42ad19658c
Marcel Haupt <subcortexx@gmail.com>
2018-05-20 09:53:10 +0200

    Impressum

--- templates/impressum.php
+++ templates/impressum.php
@@ -1,6 +1,22 @@
 <div id="page">
-  <h1>Impressum</h1>
-  <a href="#menu">Menu</a>
+  <div id="header">
 
-  <h1>Alea jacta est</h1>
-</div>
+
+    <div id="open-menu-hamburger" class="hamburger">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
+
+
+    <h1>Impressum</h1>
+  </div>
+
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-12 col-md-6 text-center">
+
+        Impressumstext hier
+      </div>
+    </div>
+  </div>
