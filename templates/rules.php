6cdfc56028ac37c1c5fdc84a94cc745579cdc9ac
Marcel Haupt <subcortexx@gmail.com>
2018-05-20 08:42:26 +0200

    Spielregeln hinzugefügt

--- templates/rules.php
+++ templates/rules.php
@@ -1,6 +1,36 @@
 <div id="page">
-  <h1>Spielregeln</h1>
-  <a href="#menu">Menu</a>
+  <div id="header">
 
-  <h1>In dubio pro reum</h1>
+
+    <div id="open-menu-hamburger" class="hamburger">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
+
+
+    <h1>Spielregeln</h1>
+  </div>
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-6">
+
+
+        <br><br>
+
+        <p>Mord In Palermo ist ein Gesellschaftsspiel. Am Anfang des Spiels werden die Spieler in verschiedene Rollen eingeteilt. Es gibt Bürger, Mörder, Ärzte und Detektive. Dabei kennt jeder Spieler nur seine eigene Rolle. Ziel des Spiels der Mörder ist, alle übrigen Spieler zu töten. Die Bürger versuchen dabei mit den Ärzten und den Detektiven die Mörder ausfindig zu machen und zu erhängen.</p>
+        <br>
+        <p>Das Spiel beginnt mit einer Diskussionsrunde, in der alle Spieler ausdiskutieren können wer als Mörder in Frage kommt und die damit endet, dass ein Spieler von den anderen “erhängt” wird. Die Mörder haben zusätzlich die Möglichkeit gemeinsam einen weiteren Spieler ihrer Wahl zu “ermorden”.</p>
+        <p>Ärzte und Detektive sind Spezialrollen und spielen auf der Seite der Bürger. Ärzte können jede Runde abstimmen, welcher Spieler von ihnen gerettet werden soll. Sollte es vorkommen, dass die Mörder den gleichen Spieler als Opfer ausgewählt haben, überlebt dieser Spieler den Angriff. Detektive dürfen jede Runde die Rolle eines Spielers ihrer Wahl erfahren.</p>
+        <p>Das Spiel ist beendet, wenn entweder alle Mörder von den übrigen Spielern “erhängt” worden sind oder wenn die Mörder es als einzige geschafft haben zu überleben.</p>
+        <br>
+        <p>Das Spiel wird in geselliger Runde in Anwesenheit aller Mitspieler gespielt. Während der Diskussionsrunde können die Spieler auf ihrem Handy oder Tablet für einen Spieler abstimmen, der anschließend “erhängt” wird und damit aus dem Spiel ausscheidet. Mörder, Ärzte und Detektive haben für ihre Rolle spezielle Funktionen auf der Benutzeroberfläche. Zusätzlich gibt es einen Chat, in dem man Nachrichten an alle, seine Rollenpartner oder andere Mitspieler schicken kann. Hauptziel der Arbeit ist die Erstellung der Software.</p>
+        <br>
+        <p>Die hitzigen Diskussionen der Spieler, die meist wilde Verschwörungstheorien und Denken um mehrere Ecken beinhalten, sind der Hauptreiz des Spiels.</p>
+
+
+      </div>
+    </div>
+  </div>
 </div>
