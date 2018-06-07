32028ce2410977db3b5443e1652739cc06b0d6c4
Marcel Haupt <subcortexx@gmail.com>
2018-06-07 15:16:55 +0200

    Settings added

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,3 +8,15 @@ function Player() {
   this.name;
   this.id;
 }
+
+
+
+$(document).ready(function() {
+  $('.slider').change(function() {
+    $(this).parent().children('.slider-text').text(this.value + ' / ' + $(this).attr('max') + ' (Standard: 2)');
+  });
+
+  $('.slider').on('input', function () {
+    $(this).trigger('change');
+  });
+});
