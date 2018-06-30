b6fefcc49fd09ed51909d10acd5475ecb1e6892f
mexwew <m.hennings@freenet.de>
2018-06-30 16:55:19 +0200

    bubbles

--- assets/js/chat_bubbles.js
+++ assets/js/chat_bubbles.js
@@ -2,16 +2,21 @@ $(document).ready(function( $ ) {
 $("#chat-send-button").click(function() {
 
 var text_me = $("#chat-input-field").val();
+if (text_me <1) {return;} 
 
 var text_somebodyelse = "Hi";
 
 var system_message = "Das ist eine Systemnachricht";
 
 var html = '<div class="chat-container-bubble-r">';
-    html += '<h5> '+text_me+'</h5>';
+    html +=   '<div class="badge badge-pill badge-primary">';
+    html +=     '<h5> '+text_me+'</h5>';
+    html +=   '</div>';
     html += '</div>';
 
+
 $("#chat-text-container").append(html);
+$("#chat-input-field").val("");
 
 })
 });
